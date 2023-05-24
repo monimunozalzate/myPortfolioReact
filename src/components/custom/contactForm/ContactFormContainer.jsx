81ed4a1e68d46fe6db42aeb437fb0c5b84c82f10
monimunozalzate <monicamunozviolin@gmail.com>
2023-05-24 15:42:42 -0500

    .

--- src/components/custom/contactForm/ContactFormContainer.jsx
+++ src/components/custom/contactForm/ContactFormContainer.jsx
@@ -1,9 +1,34 @@
 import { useFormik } from "formik";
 import ContactForm from "./ContactForm";
 import * as Yup from "yup";
-import swal from 'sweetalert';
+import swal from "sweetalert";
+// import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 
 const ContactFormContainer = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+console.log("hi from sent email")
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          swal("Good job!", "Email sent!", "success");
+        },
+        (error) => {
+          // show the user an error
+          swal("There was a mistake!", "Try again!", "error");
+        }
+      );
+  };
+
   const { values, errors, handleSubmit, handleChange } = useFormik({
     initialValues: {
       name: "",
@@ -13,7 +38,6 @@ const ContactFormContainer = () => {
     onSubmit: (data) => {
       console.log(data);
       swal("Good job!", "Your message was sent!", "success");
-      console.log("se envio el formulario");
     },
     validationSchema: Yup.object().shape({
       name: Yup.string()
@@ -34,6 +58,8 @@ const ContactFormContainer = () => {
       errors={errors}
       handleChange={handleChange}
       handleSubmit={handleSubmit}
+      form={form}
+      sendEmail={sendEmail}
     />
   );
 };
